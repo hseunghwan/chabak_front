75f2c162fe93427c86b1b5f37c3d756a130e4ae4
hseunghwan <hsh9298080@naver.com>
2023-05-14 17:56:12 +0900

    LogIn -> Login 오타 수정

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormContainer from "src/components/FormContainer";
 import SignupInput from "src/components/SignupInput";
 
-export default function LogIn() {
+export default function Login() {
     return (
         <FormContainer title="로그인">
             <SignupInput />
