d5d12c8b808b420803898bf71a355f818a437949
sjyim0513 <sjyim0513@naver.com>
2023-05-16 02:13:03 +0900

    123

--- src/chatbot/delete.py
+++ src/chatbot/delete.py
@@ -1,19 +1,20 @@
 import spacy
+from konlpy.tag import *
 
-nlp = spacy.load("ko_core_news_lg")
+hannanum = Hannanum()
+kkma = Kkma()
+komoran = Komoran()
+okt = Okt()
 
-nlp.Defaults.stop_words.add("에")
-# 불용어 추가
-custom_stop_words = ["에", "하기", "에서"]
-for word in custom_stop_words:
-    nlp.vocab[word].is_stop = True
+def preprocess_text(text):
+    # 한국어 형태소 분석 수행
+    morphs = okt.morphs(text)
+    #리스트를 문자열로 수정
+    return ' '.join(morphs)
 
-text = "경기도 봄에 가기 좋은"
+text = "경기도 이천시 겨울에 가기 좋은 곳 추천해줘"
 
-doc = nlp(text)
-
-filtered_tokens = [token for token in doc if not token.is_stop]
-# 불용어 제거
-print(filtered_tokens)
-stop_words = nlp.Defaults.stop_words
-print(stop_words)
+# 한국어 형태소 분석을 수행한 후 텍스트 전처리
+preprocessed_text = preprocess_text(text)
+print(preprocessed_text)
+print(type(preprocessed_text))
--- src/chatbot/test_ner.py
+++ src/chatbot/test_ner.py
@@ -1,22 +1,38 @@
 import spacy
+from konlpy.tag import *
+
+hannanum = Hannanum()
+kkma = Kkma()
+komoran = Komoran()
+okt = Okt()
+
+#한국어 형태소 분석
+def preprocess_text(text):
+    # 형태소 모델 사용
+    morphs = komoran.morphs(text)
+    #리스트를 문자열로 수정
+    return ' '.join(morphs)
 
 # 저장된 모델 로드
-model_path = "C:\\Users\\sunny\\Documents\\GitHub\\chabak_front\\src\\chatbot\\ner"
+model_path = "C:\\Users\\sjyim\\Documents\\GitHub\\chabak_front\\src\\chatbot\\ner"
 #nlp = spacy.load(model_path)
-nlp = spacy.load('ko_core_news_lg')
 trained_ner = spacy.load(model_path)
+nlp = spacy.load("ko_core_news_lg")
 
-# Add your trained model as a new pipeline component
-#nlp.add_pipe('custom_ner', source=trained_ner)
+input_text = "경기도 여름에 가기 좋은"
 
-# Process text with the combined model
-text = "경기도 봄 가기 좋은"
+#형태소 함수 실행
+text = preprocess_text(input_text)
+print(text)
 
+#ner 실행
 doc = nlp(text)
 doc2 = trained_ner(text)
 
+#ner 출력
 for ent in doc2.ents:
     print(ent.text, ent.label_)
 # Access the named entities
+
 for ent in doc.ents:
     print(ent.text, ent.label_)
