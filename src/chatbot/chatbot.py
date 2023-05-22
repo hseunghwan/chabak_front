66118e534a4006f256b45ff7ceab473b2c2ea7a9
sjyim0513 <sjyim0513@naver.com>
2023-05-23 04:13:10 +0900

    123

--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -15,14 +15,14 @@ class OpenAIGpt:
     #초기 설정
     def __init__(self):
         load_dotenv()
-        self.nlp = spacy.load("C:\\Users\\sunny\\Documents\\ner")
+        self.nlp = spacy.load("C:\\Users\\sjyim\\Documents\\ner")  
         self.loc = ["제주", "경기", "충청", "강원", "경상", "전라", "서울", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "시"]
-        #self.pattern1 = r'도\s\w{2,3}시'
-        #self.pattern2 = r'시\s\w{1,3}구'
+        self.pattern1 = r'도\s\w{2,3}시'
+        self.pattern2 = r'시\s\w{1,3}구'
         #사용할 형태소 분석 모델
         self.komoran = Komoran()
         #백엔드 주소
-        self.back_url = "http://example.com/upload"
+        self.back_url = "http://3.34.98.222:8080/api/chatbot/place?"
     
         #한국어 형태소 분석
     
@@ -34,17 +34,18 @@ class OpenAIGpt:
         return ' '.join(morphs)
     
     #json을 백엔드에 전송
-    #def dic_to_back(self, json_data):
+    def to_back(self, json_data):
         #json을 백엔드에 전송
-        response = requests.post(self.url, data=json_data)
+        response = requests.get(self.back_url, json=json_data)
 
         if response.status_code == 200:
             print("파일 전송 성공")
         else:
             print("파일 전송 실패")
+            print(response.status_code)
 
         #백엔드에서 받은 json 파일 리턴
-        return response.json
+        return response.json()
     
     #프론트엔드에서 prompt 받아오기
     #@app.route('/api/prompt', methods=['POST'])
@@ -67,24 +68,30 @@ class OpenAIGpt:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
         #차박지 검색용 패턴
-        #match1 = re.search(self.pattern1, prompt)
-        #match2 = re.search(self.pattern2, prompt)
+        match1 = re.search(self.pattern1, prompt)
+        match2 = re.search(self.pattern2, prompt)
 
         #차박지 관련 정보 물어보는 경우
-        if prompt in self.loc:
+        if match1 or match2 or prompt in self.loc:
             #형태소 분석
             text = self.preprocess_text(prompt, self.komoran)
             print(text)
             #분석한 text ner 처리
             doc = self.nlp(text)
-
-            entities = [(ent.text, ent.label_) for ent in doc.ents]
+            
+            entities = self.back_url+''
+            for ent in doc.ents:
+                entities += ent.label_ + "=" + ent.text + "&"
+            entities = entities[:-1]
             print(entities)
-            '''
-            dict_ = dict(entities)
-            json_data = json.dumps(dict_)
-            self.dic_to_back(json_data)
-            '''
+            json_data = json.dumps(entities)
+            a = self.to_back(json_data)
+            #print(a)
+            print(type(a))
+
+# 변환된 데이터를 확인합니다.
+
+            
             
         #백엔드에 정보 json으로 보낸 후에 프론트에는 처리 되었음을 알리는 문자를 보내야함
             
@@ -104,7 +111,6 @@ class OpenAIGpt:
               text = choice.text.strip()
               if text:
                   print(text)
-                  
 
 #최초 실행
 if __name__ == '__main__':
