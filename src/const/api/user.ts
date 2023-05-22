10ae374d217b666a7f1fa9783f64de8558375798
hseunghwan <hsh9298080@naver.com>
2023-05-22 13:00:01 +0900

    https test cdn

--- src/const/api/user.ts
+++ src/const/api/user.ts
@@ -3,7 +3,7 @@ import { apiClient } from "src/const/api/apiClient";
 export const userLogin = async (data: { email: string; password: string }) => {
     return await apiClient({
         method: "post",
-        url: `api/user/login`,
+        url: `/api/user/login`,
         data: data,
     });
 };
@@ -11,7 +11,7 @@ export const userLogin = async (data: { email: string; password: string }) => {
 export const userLogout = async (token: string | null) => {
     return await apiClient({
         method: "post",
-        url: `api/user/logout`,
+        url: `/api/user/logout`,
         headers: {
             Authorization: `Bearer ${token}`,
         },
