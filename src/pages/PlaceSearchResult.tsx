21388c87528326d9b7378990a35aaeec3f953a36
hseunghwan <hsh9298080@naver.com>
2023-05-27 04:01:01 +0900

    displayedItems.map 오류 handleApi

--- src/pages/PlaceSearchResult.tsx
+++ src/pages/PlaceSearchResult.tsx
@@ -52,8 +52,28 @@ export default function PlaceSearchResult() {
         };
     }, []);
 
-    useEffect(() => {
-        //필터에 의한 변경에 따른 상태변경, 반영
+    // useEffect(() => {
+    //     //필터에 의한 변경에 따른 상태변경, 반영
+    //     if (userSearchState.theme === null && userSearchState.facils === null && userSearchState.searchKeyword === null) {
+    //         placeListByLocation(userSearchState.location)
+    //             .then((response) => {
+    //                 setPlaceList(response.data);
+    //             })
+    //             .catch((error) => {
+    //                 console.error(error);
+    //             });
+    //     } else if (userSearchState.theme !== null && userSearchState.facils === null && userSearchState.searchKeyword === null) {
+    //         placeListByLocationTheme(userSearchState.location, userSearchState.theme)
+    //             .then((response) => {
+    //                 setPlaceList(response.data);
+    //             })
+    //             .catch((error) => {
+    //                 console.error(error);
+    //             });
+    //     }
+    // }, [setPlaceList, userSearchState]);
+
+    const handleApi = async () => {
         if (userSearchState.theme === null && userSearchState.facils === null && userSearchState.searchKeyword === null) {
             placeListByLocation(userSearchState.location)
                 .then((response) => {
@@ -71,8 +91,7 @@ export default function PlaceSearchResult() {
                     console.error(error);
                 });
         }
-    }, [setPlaceList, userSearchState]);
-
+    };
     return (
         <div style={{ backgroundColor: colors.FORMBACKGROUND }}>
             {param.showfilter === "true" && (
@@ -92,22 +111,46 @@ export default function PlaceSearchResult() {
                     </div>
                     <div style={{ display: "flex", alignContent: "center", flexGrow: 1 }}>
                         {userSearchState.location && (
-                            <span onClick={() => setUserSearchState({ ...userSearchState, location: "전국" })} style={spanStyle}>
+                            <span
+                                onClick={() => {
+                                    setUserSearchState({ ...userSearchState, location: "전국" });
+                                    handleApi();
+                                }}
+                                style={spanStyle}
+                            >
                                 지역:{userSearchState.location}
                             </span>
                         )}
                         {userSearchState.theme && (
-                            <span onClick={() => setUserSearchState({ ...userSearchState, theme: null })} style={spanStyle}>
+                            <span
+                                onClick={() => {
+                                    setUserSearchState({ ...userSearchState, theme: null });
+                                    handleApi();
+                                }}
+                                style={spanStyle}
+                            >
                                 테마:{userSearchState.theme}
                             </span>
                         )}
                         {userSearchState.facils && (
-                            <span onClick={() => setUserSearchState({ ...userSearchState, facils: null })} style={spanStyle}>
+                            <span
+                                onClick={() => {
+                                    setUserSearchState({ ...userSearchState, facils: null });
+                                    handleApi();
+                                }}
+                                style={spanStyle}
+                            >
                                 시설:{userSearchState.facils}
                             </span>
                         )}
                         {userSearchState.searchKeyword && (
-                            <span onClick={() => setUserSearchState({ ...userSearchState, searchKeyword: null })} style={spanStyle}>
+                            <span
+                                onClick={() => {
+                                    setUserSearchState({ ...userSearchState, searchKeyword: null });
+                                    handleApi();
+                                }}
+                                style={spanStyle}
+                            >
                                 검색:{userSearchState.searchKeyword}
                             </span>
                         )}
