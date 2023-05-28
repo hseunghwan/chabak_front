2f14febc6881f153792f3c8bb7dd7221b7053b37
hseunghwan <hsh9298080@naver.com>
2023-05-28 23:42:30 +0900

    user place 주소 api 해야됨

---
+++ src/const/api/userPlace.ts
@@ -0,0 +1,18 @@
+import { apiClient } from "src/const/api/apiClient";
+export type UserPlaceRegisterModel = {
+    address: string;
+    userPlaceName: string;
+    descript: string;
+    tags: string;
+    latitude: string;
+    longitude: string;
+};
+
+//사용자 장소 등록
+export const userPlaceRegister = async (data: UserPlaceRegisterModel) => {
+    return await apiClient({
+        method: "post",
+        url: `/api/register/address`,
+        data: data,
+    });
+};
---
+++ src/pages/RegisterPlaceByAddress.tsx
@@ -0,0 +1,40 @@
+import { Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import { CustomInput } from "src/components/SimpleInput";
+
+import { userPlaceRegister, UserPlaceRegisterModel } from "src/const/api/userPlace";
+
+export default function RegisterPlaceByAddress() {
+    const [address, setAddress] = useState("");
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
+    const [userPlaceName, setUserPlaceName] = useState("");
+    const [descript, setDescript] = useState("");
+    const [tags, setTags] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const setData: UserPlaceRegisterModel = { address, userPlaceName, descript, tags, latitude, longitude };
+        userPlaceRegister(setData)
+            .then((response) => {
+                console.log("Register success");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    return (
+        <Box component={"form"} onSubmit={handleSubmit} sx={{ display: "flex", flexWrap: "wrap", flexDirection: "column", alignContent: "center" }}>
+            <span style={{ fontSize: "1.5rem" }}>주소로 위치 선택</span>
+            {/* 지도, 주소 api */}
+
+            <span style={{ fontSize: "0.8rem", marginLeft: "1rem", marginTop: "1rem" }}>차박지 이름</span>
+            <CustomInput value={userPlaceName} onChange={(e) => setUserPlaceName(e.target.value)} />
+            <span style={{ fontSize: "0.8rem", marginLeft: "1rem", marginTop: "1rem" }}>차박지 설명</span>
+            <CustomInput value={descript} onChange={(e) => setDescript(e.target.value)} sx={{ height: "200px", overflow: "auto" }} />
+            <span style={{ fontSize: "0.8rem", marginLeft: "1rem", marginTop: "1rem" }}>해시태그</span>
+            <CustomInput value={tags} onChange={(e) => setTags(e.target.value)} />
+            <Button type="submit">등록</Button>
+        </Box>
+    );
+}
