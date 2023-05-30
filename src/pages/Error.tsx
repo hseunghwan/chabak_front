f1de79ebebe2b739ce2c7344fca73c8bc3a1c071
hseunghwan <hsh9298080@naver.com>
2023-05-30 14:29:47 +0900

    로그인 백 에러

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FormContainer from "src/components/FormContainer";
 
 export default function Error() {
-    return <FormContainer title="ERROR"> Error </FormContainer>;
+    useEffect(() => {
+        window.location.reload();
+    }, []); // useEffect will run once when component is mounted
+    return <FormContainer title=" "> </FormContainer>;
 }
