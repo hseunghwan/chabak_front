a0906592731cf546322bc990e139355cef74bbac
hseunghwan <hsh9298080@naver.com>
2023-05-30 10:13:34 +0900

    FNM ?

--- src/components/FloatingNaverMap.tsx
+++ src/components/FloatingNaverMap.tsx
@@ -57,18 +57,19 @@ function MarkerCluster() {
         }
         if (placeList === null || placeList === undefined) return;
         const markers: naver.maps.Marker[] = [];
-        // eslint-disable-next-line array-callback-return
-        placeList.map((place) => {
-            let lat = place.latitude === null ? false : Number(place.latitude);
-            let lng = place.longitude === null ? false : Number(place.longitude);
-            if (lat !== false && lng !== false) {
-                let marker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(lng, lat),
-                });
-                markers.push(marker);
-            }
-        });
-
+        if (Array.isArray(placeList)) {
+            // eslint-disable-next-line array-callback-return
+            placeList.map((place) => {
+                let lat = place.latitude === null ? false : Number(place.latitude);
+                let lng = place.longitude === null ? false : Number(place.longitude);
+                if (lat !== false && lng !== false) {
+                    let marker = new naver.maps.Marker({
+                        position: new naver.maps.LatLng(lng, lat),
+                    });
+                    markers.push(marker);
+                }
+            });
+        }
         const newCluster = new MarkerClustering({
             minClusterSize: 2,
             maxZoom: 15,
