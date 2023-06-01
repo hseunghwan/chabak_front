4518734c819300dc6bddc1a59c364b3d5a16202c
hseunghwan <hsh9298080@naver.com>
2023-06-01 19:55:39 +0900

    /cdn /cdn /cdn/cdn

--- src/const/api/apiClient.ts
+++ src/const/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BACKEND_URL } from "src/const/consts";
 
-const host = window.location.hostname === "localhost" ? BACKEND_URL : "cdn";
+const host = window.location.hostname === "localhost" ? BACKEND_URL : "/cdn";
 
 export const apiClient = axios.create({
     baseURL: host,
