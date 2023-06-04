67e53ac1934320fdb2d4dce112a17c89ed349840
hseunghwan <hsh9298080@naver.com>
2023-06-04 14:13:05 +0900

    모바일 화면 지도, 채팅 개선

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,12 +63,12 @@ export default function Home() {
 
     const homeContainerStyles: SxProps<Theme> = {
         position: "relative",
-        left: { xs: undefined, sm: undefined, md: showSpeechBubble ? "25%" : 0 },
         transition: "left 0.5s",
         flexDirection: "column",
         width: "100%",
         [theme.breakpoints.up("md")]: {
             width: "45%",
+            left: showSpeechBubble ? "25%" : 0,
         },
         height: "100%",
     };
